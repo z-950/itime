231dc0a654fc79888d7a3b4e9e57120a215b74c2
950 <1033766950@qq.com>
2019-11-06 00:20:45 +0800

    添加about

---
+++ app/src/main/java/com/example/casper/itime/AboutFragment.java
@@ -0,0 +1,26 @@
+package com.example.casper.itime;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AboutFragment extends Fragment {
+
+    public AboutFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
--- app/src/main/java/com/example/casper/itime/MainActivity.java
+++ app/src/main/java/com/example/casper/itime/MainActivity.java
@@ -13,15 +13,24 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String HOME_TAG = "home";
+    private static final String ABOUT_TAG = "about";
+
+
+    private FragmentManager fragmentManager;
+    private String currentTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -32,11 +41,41 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
+        fragmentManager = getSupportFragmentManager();
         initRoutes();
+        showRoute(HOME_TAG);
     }
 
-    private void initRoutes(){
-        getSupportFragmentManager().beginTransaction().add(R.id.main_frame_layout, new HomeFragment(), "home").commit();
+    private void initRoutes() {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        initAddRoute(transaction, new HomeFragment(), HOME_TAG);
+        initAddRoute(transaction, new AboutFragment(), ABOUT_TAG);
+
+        transaction.commitNow();
+    }
+
+    private void initAddRoute(FragmentTransaction transaction, Fragment fragment, String tab) {
+        transaction.add(R.id.main_frame_layout, fragment, tab);
+        transaction.hide(fragment);
+    }
+
+    private void showRoute(String tag) {
+        if (tag == currentTag) {
+            return;
+        }
+
+        Fragment fragmentOld = fragmentManager.findFragmentByTag(currentTag);
+        Fragment fragmentNew = fragmentManager.findFragmentByTag(tag);
+
+        if (fragmentNew != null) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            if (fragmentOld != null) {
+                transaction.hide(fragmentOld);
+            }
+            transaction.show(fragmentNew).commit();
+            currentTag = tag;
+        }
     }
 
     @Override
@@ -49,14 +88,12 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            showRoute(HOME_TAG);
         } else if (id == R.id.nav_widget) {
 
         } else if (id == R.id.nav_theme) {
@@ -66,7 +103,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_about) {
-
+            showRoute(ABOUT_TAG);
         } else if (id == R.id.nav_report) {
 
         }
