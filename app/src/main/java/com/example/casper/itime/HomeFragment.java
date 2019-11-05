4fa3e35b3e4f00b4d6728f7e724dba21caea95db
950 <1033766950@qq.com>
2019-11-06 01:43:02 +0800

    设置颜色

--- app/src/main/java/com/example/casper/itime/HomeFragment.java
+++ app/src/main/java/com/example/casper/itime/HomeFragment.java
@@ -1,7 +1,9 @@
 package com.example.casper.itime;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,6 +14,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 public class HomeFragment extends Fragment {
+    FloatingActionButton fab;
+    int color = 0;
+    boolean colorChanged = false;
+    boolean setable = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,7 +25,7 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        FloatingActionButton fab = view.findViewById(R.id.fab);
+        fab = view.findViewById(R.id.add_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,6 +34,38 @@ public class HomeFragment extends Fragment {
             }
         });
 
-        return  view;
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setable = true;
+        if (colorChanged) {
+            this.colorChanged = false;
+            setFabColor();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        setable = false;
+    }
+
+    public void setColor(int color) {
+        if (this.color != color) {
+            colorChanged = true;
+            this.color = color;
+
+            if (this.setable) {
+                this.colorChanged = false;
+                setFabColor();
+            }
+        }
+    }
+
+    private void setFabColor(){
+        fab.setBackgroundTintList(ColorStateList.valueOf(color));
     }
 }
--- app/src/main/java/com/example/casper/itime/MainActivity.java
+++ app/src/main/java/com/example/casper/itime/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.casper.itime;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -22,17 +26,22 @@ public class MainActivity extends AppCompatActivity
     private static final String HOME_TAG = "home";
     private static final String ABOUT_TAG = "about";
 
-
     private FragmentManager fragmentManager;
     private String currentTag;
+
+    private Toolbar toolbar;
+
+    private int themeColor = 0xFF000000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.tool_bar);
+        // 初始化工具栏
+        toolbar = findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
+        // 初始化抽屉
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -41,9 +50,36 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
+        // 初始化fragment
         fragmentManager = getSupportFragmentManager();
         initRoutes();
         showRoute(HOME_TAG);
+
+        // 初始化主题颜色
+        setThemeColor(themeColor);
+    }
+
+    void setThemeColor(int color) {
+        themeColor = color;
+        // 状态栏颜色
+        toolbar.setBackgroundColor(color);
+        setStatusBarColor(color);
+        // 按钮颜色
+        ((HomeFragment) fragmentManager.findFragmentByTag(HOME_TAG)).setColor(color);
+    }
+
+    void setStatusBarColor(int statusColor) {
+        Window window = this.getWindow();
+        //取消状态栏透明
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //添加Flag把状态栏设为可绘制模式
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        //设置状态栏颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(statusColor);
+        }
+        //设置系统状态栏处于可见状态
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
     }
 
     private void initRoutes() {
@@ -70,10 +106,11 @@ public class MainActivity extends AppCompatActivity
 
         if (fragmentNew != null) {
             FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.show(fragmentNew);
             if (fragmentOld != null) {
                 transaction.hide(fragmentOld);
             }
-            transaction.show(fragmentNew).commit();
+            transaction.commit();
             currentTag = tag;
         }
     }
